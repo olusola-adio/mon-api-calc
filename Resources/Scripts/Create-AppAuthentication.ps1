39c0720c044ece8fbcb186573214535063db5bc2
olusola-adio <olusola_adio@hotmail.com>
2021-03-21 13:47:07 +0000

    create sp

--- Resources/Scripts/Create-AppAuthentication.ps1
+++ Resources/Scripts/Create-AppAuthentication.ps1
@@ -45,13 +45,19 @@ try {
         Write-Verbose "appId doesnt exist, so creatinbg a new one"
         az ad app create --display-name $AADappName --homepage="https://$($WebAppFDQN)" --reply-urls $urls --oauth2-allow-implicit-flow true
 
+
         Write-Verbose "Get appp id"
         $AADappId = $(az ad app list --display-name $AADappName --query [].appId -o tsv)
         $MSGraphAPI = "00000003-0000-0000-c000-000000000000" #UID of Microsoft Graph
         $Permission = "e1fe6dd8-ba31-4d61-89e7-88639da4683d=Scope" # ID: Read permission, Type: Scope
-    
+
+        Write-Verbose "Create associated service principal" 
+        az ad sp create --id $AADappId
+
+
         Write-Verbose "set app permission"
         az ad app permission add --id $AADappId --api $MSGraphAPI --api-permissions $Permission
+        az ad app permission grant --id $AADappId --api $MSGraphAPI
     }
 
 
