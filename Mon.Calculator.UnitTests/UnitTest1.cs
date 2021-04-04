8cce3cbe27031f3b5c6ae447882ed7d63bc629be
olusola-adio <olusola_adio@hotmail.com>
2021-04-05 00:09:48 +0100

    tests

--- Mon.Calculator.UnitTests/UnitTest1.cs
+++ Mon.Calculator.UnitTests/UnitTest1.cs
@@ -1,14 +1,21 @@
-using System;
+using System.Net;
 using Xunit;
 
 namespace Mon.Calculator.UnitTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+       [Fact]
+        public void GetDetailHttpTriggerRunReturnsSuccess()
         {
+            // Arrange
+            const HttpStatusCode expectedResult = HttpStatusCode.OK;
 
+            // Act
+            
+
+            // Assert
+            Assert.Equal((int)expectedResult, (int)expectedResult);
         }
     }
 }
