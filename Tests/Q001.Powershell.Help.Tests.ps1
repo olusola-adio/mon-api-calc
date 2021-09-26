d72ab24262780ebc0d1bb29413795ff0a69e79ea
olusola-adio <olusola_adio@hotmail.com>
2021-09-26 20:03:05 +0100

    refine pester tests

--- Tests/Q001.Powershell.Help.Tests.ps1
+++ Tests/Q001.Powershell.Help.Tests.ps1
@@ -1,69 +1,80 @@
-Describe "PowerShell script help quality tests" -Tag "Quality" {
+<#
+.SYNOPSIS
+Runs the Help Quality tests
 
-    BeforeDiscovery {
-        $Scripts = Get-ChildItem -Path $PSScriptRoot\..\*.ps1 -File -Recurse | Where-Object { $_.FullName -notlike "*\tests\*" }
-        Write-Output $Scripts
-    }
+.DESCRIPTION
+Runs the Help Quality tests
 
-    Context "<_.BaseName>" -Foreach @($Scripts) {
+.EXAMPLE
+Q001.Powershell.Help.Tests.ps1
 
-        BeforeDiscovery {
-            $help = Get-Help $_.FullName
-            $parameters = @()
-            Write-Output $parameters.Count
-            if ($help.parameters.parameter.count -ne 0) {
-                $parameters = $help.Parameters.Parameter 
-            }
-        }
-
-        It "Should have a Synopsis" -Foreach @{help = $help}{
-            $help.Synopsis | Should -Not -BeNullOrEmpty
-        }
-        It "Should have a Description" -Foreach @{help = $help}{
-            $help.Description | Should -Not -BeNullOrEmpty
-        }
-        It "Should have an Example" -Foreach @{help = $help}{
-            $help.examples | Select-Object -First 1 | Should -HaveCount 1
-        }                
-        It "Should have a Parameter description for <_.Name>" -ForEach @($parameters) {
-            $_.Description.Text | Should -Not -BeNullOrEmpty
-        }
-    }
-}
-
-
-
-# BeforeDiscovery {
-#     $Scripts = Get-ChildItem -Path $PSScriptRoot\..\*.ps1 -File -Recurse | Where-Object { $_.FullName -notlike "*\tests\*" }
-#     Write-Host "File count discovered for Help quality Tests: $($Scripts.Count)"
-# }
-# Describe "Help quality tests for '<_>'" -ForEach @($Scripts) -Tag "Quality" {
+#>
+# Describe "PowerShell script help quality tests" -Tag "Quality" {
 
 #     BeforeDiscovery {
-#         $scriptName = $_.FullName
-#         $help = Get-Help $_.FullName
-#         $parameters = @()
-#         if ($help.parameters.parameter.count -ne 0) {
-#             $parameters = $help.Parameters.Parameter 
-#         }
+#         $Scripts = Get-ChildItem -Path $PSScriptRoot\..\*.ps1 -File -Recurse | Where-Object { $_.FullName -notlike "*/tests/*" }
+#         Write-Host "File count discovered for Help quality Tests: $($Scripts.Count)"
 #     }
 
-#     Context "Test $scriptName for basic help" -Foreach @{help = $help } {
+#     Context "<_.BaseName>" -Foreach @($Scripts) {
 
-#         It "Should have a Synopsis" {
+#         BeforeDiscovery {
+#             $help = Get-Help $_.FullName
+#             $parameters = @()
+#             Write-Output $parameters.Count
+#             if ($help.parameters.parameter.count -ne 0) {
+#                 $parameters = $help.Parameters.Parameter
+#             }
+#         }
+
+#         It "Should have a Synopsis" -Foreach @{help = $help}{
 #             $help.Synopsis | Should -Not -BeNullOrEmpty
 #         }
-#         It "Should have a Description" {
+#         It "Should have a Description" -Foreach @{help = $help}{
 #             $help.Description | Should -Not -BeNullOrEmpty
 #         }
-#         It "Should have an Example" {
+#         It "Should have an Example" -Foreach @{help = $help}{
 #             $help.examples | Select-Object -First 1 | Should -HaveCount 1
-#         }                
-#     }
-       
-#     Context "Parameter Definition for $scriptName" -Foreach @{parameters = $parameters } {
+#         }
 #         It "Should have a Parameter description for <_.Name>" -ForEach @($parameters) {
 #             $_.Description.Text | Should -Not -BeNullOrEmpty
 #         }
 #     }
 # }
+
+BeforeDiscovery {
+    $Scripts = Get-ChildItem -Path $PSScriptRoot\..\*.ps1 -File -Recurse
+    Write-Host "File count discovered for Help quality Tests: $($Scripts.Count)"
+}
+Describe "Help quality tests for '<_>'" -ForEach @($Scripts) -Tag "Quality" {
+
+    BeforeDiscovery {
+        $scriptName = $_.FullName
+        Write-Output "Script name: $($scriptName)"
+        $help = Get-Help $_.FullName
+        $parameters = @()
+        if ($help.parameters.parameter.count -ne 0) {
+            $parameters = $help.Parameters.Parameter
+        }
+        Write-Output "parameter count: $($parameters.Count)"
+    }
+
+    Context "Test $scriptName for basic help" -Foreach @{help = $help } {
+
+        It "Should have a Synopsis" {
+            $help.Synopsis | Should -Not -BeNullOrEmpty
+        }
+        It "Should have a Description" {
+            $help.Description | Should -Not -BeNullOrEmpty
+        }
+        It "Should have an Example" {
+            $help.examples | Select-Object -First 1 | Should -HaveCount 1
+        }
+    }
+
+    Context "Parameter Definition for $scriptName" -Foreach @{parameters = $parameters } {
+        It "Should have a Parameter description for <_.Name>" -ForEach @($parameters) {
+            $_.Description.Text | Should -Not -BeNullOrEmpty
+        }
+    }
+}
